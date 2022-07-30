f9bcb8f5366ad92210a51f2e4b3be8374fff7432
Kim Jisena <pjohmaths@gmail.com>
2022-07-30 09:57:20 +0300

    replace var declarations with const

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
-//Set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://kimjisena:jt31x7FfLRnU5iwD@clustero.pkemxgq.mongodb.net/local_library?retryWrites=true&w=majority';
+// set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb+srv://kimjisena:jt31x7FfLRnU5iwD@clustero.pkemxgq.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
