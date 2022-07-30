9b00264fb8f9a4b29c313164dfa3e734570246ec
Kim Jisena <pjohmaths@gmail.com>
2022-07-30 09:02:46 +0300

    add mongoose

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET some coool shit */
+router.get('/cool', function(req, res, next) {
+  res.send('this is some cool shit!');
+});
+
 module.exports = router;
