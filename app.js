43f89dc54a55abf60f39ffd9e004b391cb48770c
Kim Jisena <pjohmaths@gmail.com>
2022-07-31 02:02:21 +0300

    damn, thanks gitgurdian

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 // set up mongoose connection
-const uri = 'mongodb+srv://kimjisena:jt31x7FfLRnU5iwD@clustero.pkemxgq.mongodb.net/local_library?retryWrites=true&w=majority';
+const uri = '';
 mongoose.connect(uri, { useNewUrlParser: true , useUnifiedTopology: true});
 const dbConnection = mongoose.connection;
 dbConnection.on('error', console.error.bind(console, 'MongoDB connection error:'));
